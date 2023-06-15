249f673cd33466e1bf897bd033bce64c401e1304
Freddy Torrealba <39941253+fredvella@users.noreply.github.com>
2023-06-15 17:25:14 +0200

    Update build-images.sh

--- .github/scripts/build-images.sh
+++ .github/scripts/build-images.sh
@@ -5,6 +5,6 @@ image_paths=$(find . -type f -name $to_find)
 i=0
 for image in $image_paths; do
         image_path=${image%"Dockerfile"}
-        docker build -t img-$i:${{ github.sha }} $image_path
+        docker build -t img-${i}:${{ github.sha }} $image_path
         i=$((i+1))
 done
